7d3b16540cc9f96fa63b1661d1958c8d03de63ef
Yelyzaveta Baranovska <lizabaranovska133@gmail.com>
2025-10-02 17:49:15 +0300

    add js css

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const API_KEY = '52575025-ab19d41e2843e5421c049623c';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export async function getImagesByQuery(query) {
+  try {
+    const response = await axios.get(BASE_URL, {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error('Failed to fetch images');
+  }
+}
---
+++ src/js/render-functions.js
@@ -0,0 +1,42 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+let lightbox = new SimpleLightbox('.gallery a');
+
+const galleryContainer = document.querySelector('.gallery');
+const loader = document.getElementById('loader');
+
+export function createGallery(images) {
+  const markup = images
+    .map(image => {
+      return `
+      <li class="gallery-item">
+        <a href="${image.largeImageURL}">
+          <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+        </a>
+        <div class="image-stats">
+          <span>👍 ${image.likes}</span>
+          <span>👁️ ${image.views}</span>
+          <span>💬 ${image.comments}</span>
+          <span>⬇️ ${image.downloads}</span>
+        </div>
+      </li>
+    `;
+    })
+    .join('');
+
+  galleryContainer.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
+}
+
+export function clearGallery() {
+  galleryContainer.innerHTML = '';
+}
+
+export function showLoader() {
+  loader.classList.remove('is-hidden');
+}
+
+export function hideLoader() {
+  loader.classList.add('is-hidden');
+}
